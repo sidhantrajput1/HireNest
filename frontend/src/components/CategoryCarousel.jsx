b497c57cd1c239a762f67ed5de2d2bff5da976d1
sidhantrajput1 <sidhant808310@gmail.com>
2024-12-01 23:44:20 +0530

    CategoryCarousel

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,9 +1,46 @@
+import { Button } from "./ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+
+const catogry = [
+  "Full Stack Developer",
+  "Frontend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "UI Designer",
+  "Backend Developer",
+  "DevOps Engineer",
+  "Mobile App Developer",
+  "Cybersecurity Specialist",
+  "Cloud Architect",
+  "AI/ML Engineer",
+  "Blockchain Developer",
+  "Database Administrator",
+  "Game Developer",
+  "Technical Writer",
+];
+
 function CategoryCarousel() {
-    return (
-        <div className="max-w-screen-xl mx-auto mt-10">
-            <h3>halo</h3>
-        </div>
-    )
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <Carousel className="w-full mx-auto my-14 max-w-xl">
+        <CarouselContent>
+          {catogry.map((cat, i) => (
+            <CarouselItem  className="pl-2 md:basis-1/2 lg:basis-1/3" key={i}>
+              <Button variant="outline">{cat}</Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
 
-export default CategoryCarousel
+export default CategoryCarousel;
