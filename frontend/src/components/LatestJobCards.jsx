a0f6787f60ca320566a41230460316852dedeb91
sidhantrajput1 <sidhant808310@gmail.com>
2024-12-03 22:38:41 +0530

    Job List

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -4,18 +4,24 @@ function LatestJobCards() {
   return (
     <div className="shadow hover:shadow-xl shadow-cyan-400/20 flex flex-col border border-spacing-2 gap-4 mix-blend-multiply p-5 mt-4 rounded-xl m-3">
       <div>
-        <h2>Microsoft Private Limited</h2>
-        <p>India 📍</p>
+        <h2 className="font-medium text-lg">Microsoft Private Limited</h2>
+        <p className="text-sm text-gray-500">India 📍</p>
       </div>
       <div className="flex gap-1 flex-col">
-        <h3>MERN Stack Developer</h3>
+        <h3 className="font-medium text-pretty text-base">MERN Stack Developer</h3>
         <p>
           Proficient Experience Using Frontend Frameworks - React js, Next.js
         </p>
       </div>
-      <div>
+      <div className="flex gap-2">
         <Badge className="font-serif font-bold text-blue-600" variant="ghost">
           12 Postions
+        </Badge>
+        <Badge className="font-serif font-bold text-[#F83002]" variant="ghost">
+          Part Time
+        </Badge>
+        <Badge className="font-serif font-bold text-[#7209b7]" variant="ghost">
+          24 LPA
         </Badge>
       </div>
     </div>
